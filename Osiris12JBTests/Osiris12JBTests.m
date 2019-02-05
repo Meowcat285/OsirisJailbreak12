b1dd8bbde522766069121db7aec67a9c9212d7f9
Meowcat285 <alexschmitt@kims-imac-2>
2019-02-04 17:37:42 -0800

    Fixed auto build

---
+++ Osiris12JBTests/Osiris12JBTests.m
@@ -0,0 +1,36 @@
+//
+//  Osiris12JBTests.m
+//  Osiris12JBTests
+//
+//  Created by Alex Schmitt on 2/4/19.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Osiris12JBTests : XCTestCase
+
+@end
+
+@implementation Osiris12JBTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
